8d9ea4f54d8064311ad76fbbfa6235eb1f00b2a1
18050674 <18050674@myrp.edu.sg>
2021-04-22 14:33:46 +0800

    Updated

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,10 +21,14 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv = findViewById(R.id.lvYear);
         al = new ArrayList<String>();
         al.add("Year 1");
         al.add("Year 2");
         al.add("Year 3");
+
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,al);
+        lv.setAdapter(aa);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
